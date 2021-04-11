436401b2d4ed8e5931b494caf42a23e3c6f28c43
IrinaMak13 <82171797+IrinaMak13@users.noreply.github.com>
2021-04-11 13:20:37 +0300

    добавление классов Rectangle и Square

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,22 +7,20 @@ public class MyFirstProgram {
         hello("user");
         hello("Irina");
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+        Rectangle r = new Rectangle (4,6);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+        Point p1 = new Point (6,7);
+        Point p2 = new Point (8,9);
+        System.out.println("Расстояние между точкой с координатами "+ p1.x + " и " + p1.y + " и второй точкой с координатами " + p2.x + "  и " + p2.y + " = " + p1.distance(p2));
     }
 
     public static void hello (String somebody) {
         System.out.println("Hello," + somebody + "!");
     }
-    public static double area (double len) {
-        return len*len;
-    }
 
-    public static double area (double a, double b) {
-        return a * b;
-    }
+
 }
