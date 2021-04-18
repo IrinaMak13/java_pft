3ff0dc1b6ba7a941bc26853a77513240cbcec0ed
IrinaMak13 <82171797+IrinaMak13@users.noreply.github.com>
2021-04-18 11:46:36 +0300

    contactHelper добавлен к базовому классу

--- addressbook-web-tests/src/test/java/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/appmanager/ContactHelper.java
@@ -4,37 +4,29 @@ import model.ContactData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ContactHelper {
-  private FirefoxDriver wd;
+public class ContactHelper extends HelperBase {
 
-  public ContactHelper (FirefoxDriver wd) {this. wd = wd;}
+  public ContactHelper (FirefoxDriver wd) {
+    super(wd);}
 
-  public void returnToHomePage() {wd.findElement(By.linkText("home page")).click();
+  public void returnToHomePage() {
+    click(By.linkText("home page"));
   }
 
   public void submitContactCreation() {
-    wd.findElement(By.name("theform")).click();
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+   click(By.name("theform"));
+   click (By.xpath("(//input[@name='submit'])[2]"));
   }
 
   public void fillContactForm(ContactData contactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-    wd.findElement(By.name("theform")).click();
-    wd.findElement(By.name("theform")).click();
-    wd.findElement(By.name("theform")).click();
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
+    type (By.name("firstname"), contactData.getName());
+    type (By.name("lastname"), contactData.getLastName());
+    click (By.name("theform"));
+    type (By.name("address"), contactData.getAddress());
+    type (By.name("mobile"), contactData.getMobile());
   }
 
-  public void goToAddNewContactPage() {wd.findElement(By.linkText("add new")).click();
+  public void goToAddNewContactPage() {
+    click(By.linkText("add new"));
   }
 }
