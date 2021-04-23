43f957db1f5e6a362cfbf7e422cc88f529de14c1
IrinaMak13 <82171797+IrinaMak13@users.noreply.github.com>
2021-04-23 16:36:13 +0300

    запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/tests/TestBase.java
@@ -1,15 +1,16 @@
 package tests;
 
 import appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-  protected final ApplicationManager app;
+  public ApplicationManager app;
 
-  {
-    app = new ApplicationManager();
+  public TestBase() {
+    app = new ApplicationManager(BrowserType.CHROME);
   }
 
   @BeforeMethod(alwaysRun = true)
