c325a1c04a3c24ecea03aa627fc6c999eee65734
IrinaMak13 <82171797+IrinaMak13@users.noreply.github.com>
2021-04-24 19:04:23 +0300

    Проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/tests/ContactModificationTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import model.ContactData;
+import model.GroupData;
 import org.testng.annotations.Test;
 
 public class ContactModificationTests extends TestBase {
@@ -8,6 +9,9 @@ public class ContactModificationTests extends TestBase {
   @Test
   public void testContactModification () {
     app.getNavigationHelper().gotoHomePage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("name1", null, null));
+    }
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillContactForm(new ContactData("Name2", "Last Name 2", "Address2", "123456789", null), false);
     app.getContactHelper().submitContactModification();
--- addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import model.GroupData;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase{
@@ -7,6 +8,9 @@ public class GroupDeletionTests extends TestBase{
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().goToGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("name1", "header1", "footer1"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
