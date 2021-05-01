057fb3b660ebe1b612e8d3edeec187116389a858
IrinaMak13 <82171797+IrinaMak13@users.noreply.github.com>
2021-05-01 21:35:23 +0300

    сравнение коллекций группы

--- addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
@@ -1,8 +1,8 @@
 package tests;
 
-import org.testng.Assert;
-import org.testng.annotations.*;
 import model.GroupData;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.util.List;
 
