26f3d84f180df9715b2f0562a7b3c064da7ff578
IrinaMak13 <82171797+IrinaMak13@users.noreply.github.com>
2021-05-01 19:52:54 +0300

    выбор элемента из списка

--- addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupDeletionTests.java
@@ -13,8 +13,9 @@ public class GroupDeletionTests extends TestBase{
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("name1", "header1", "footer1"));
     }
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before-1);
     app.getGroupHelper().deleteSelectedGroups();
+    app.getNavigationHelper().goToGroupPage();
     int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after, before-1);
     app.getGroupHelper().returnToGroupPage();
--- addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase {
   public void testGroupModification() {
     app.getNavigationHelper().goToGroupPage();
     int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().selectGroup(before-1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("name1", "header1", "footer1"));
     app.getGroupHelper().submitGroupModification();
