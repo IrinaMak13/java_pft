d200c2382b05f854652709ba0c413b909ddc3076
IrinaMak13 <82171797+IrinaMak13@users.noreply.github.com>
2021-05-14 12:49:36 +0300

    GroupCreationTests- формат xml

--- addressbook-web-tests/src/test/java/model/GroupData.java
+++ addressbook-web-tests/src/test/java/model/GroupData.java
@@ -1,8 +1,18 @@
 package model;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.util.Objects;
+@XStreamAlias("group")
 
 public class GroupData {
+  @XStreamOmitField
+  private int id = Integer.MAX_VALUE;
+  private  String name;
+  private  String header;
+  private  String footer;
 
   public GroupData withId(int id) {
     this.id = id;
@@ -28,10 +38,7 @@ public class GroupData {
     return this;
   }
 
-  private int id = Integer.MAX_VALUE;
-  private  String name;
-  private  String header;
-  private  String footer;
+
 
 
   public String getName() {
