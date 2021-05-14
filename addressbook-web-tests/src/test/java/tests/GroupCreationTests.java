b916d90310453d40f12e724ba64eb4f5fb4488e8
IrinaMak13 <82171797+IrinaMak13@users.noreply.github.com>
2021-05-14 10:47:10 +0300

    GroupTests-6.1-6.3

--- addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/tests/GroupCreationTests.java
@@ -2,22 +2,38 @@ package tests;
 
 import model.GroupData;
 import model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
-    app.goTo().groupPage();
-    Groups before = app.group().all();
-    GroupData group = new GroupData().withName("name1").withHeader("header1").withFooter("footer1");
-    app.group().create(group);
-    assertThat(app.group().count(),equalTo(before.size ()+1));
-    Groups after = app.group().all();
-    assertThat(after, equalTo(
-            before.withAdded( group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("name1").withHeader("header1").withFooter("footer1")});
+    list.add(new Object[] {new GroupData().withName("name2").withHeader("header2").withFooter("footer2")});
+    list.add(new Object[] {new GroupData().withName("name3").withHeader("header3").withFooter("footer3")});
+    return list.iterator();
+  }
+
+
+  @Test(dataProvider ="validGroups")
+  public void testGroupCreation(GroupData group) {
+      app.goTo().groupPage();
+      Groups before = app.group().all();
+      app.group().create(group);
+      assertThat(app.group().count(), equalTo(before.size() + 1));
+      Groups after = app.group().all();
+      assertThat(after, equalTo(
+              before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+
 
   }
 
