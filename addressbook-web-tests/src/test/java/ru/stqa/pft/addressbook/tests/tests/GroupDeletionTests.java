56a4f73bf64966b096ad5ee67959c45804f3930b
IrinaMak13 <82171797+IrinaMak13@users.noreply.github.com>
2021-05-21 22:03:08 +0300

    GroupDeletion-равнение данных, загруженных из БД2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/tests/GroupDeletionTests.java
@@ -13,9 +13,9 @@ public class GroupDeletionTests extends TestBase{
 
   @BeforeMethod
   public void ensurePreconditions() {
-    app.goTo().groupPage();
-    if (app.group().all().size() == 0) {
-      app.group().create(new GroupData().withName("test1"));
+   if (app.db().groups().size()==0) {
+     app.goTo().groupPage();
+     app.group().create(new GroupData().withName("name1"));
     }
   }
 
@@ -23,6 +23,7 @@ public class GroupDeletionTests extends TestBase{
   public void testGroupDeletion() {
     Groups before = app.db().groups();
     GroupData deletedGroup = before.iterator().next();
+    app.goTo().groupPage();
     app.group().delete(deletedGroup);
     assertThat(app.group().count(),equalTo(before.size ()-1));
     Groups after = app.db().groups();
